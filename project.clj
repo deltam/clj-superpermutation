f7756648a693519d06b5f4b10f7ffcb69a31b796
MISUMI Masaru <deltam@users.noreply.github.com>
2019-06-11 16:18:07 +0900

    Update license

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject superm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Find minimal super permutation"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License"
+            :url "none"
+            :year 2019
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.5"]
                  [org.clojure/core.async "0.4.490"]]
