36a1ab089e053c4f463662578a79b921d4f22db9
MISUMI Masaru <deltam@users.noreply.github.com>
2019-12-04 01:43:34 +0900

    Rewrite find-superperm

--- src/superperm/core.clj
+++ src/superperm/core.clj
@@ -1,6 +1,7 @@
 (ns superperm.core
   "Find minimal super permutations"
-  (:require [clojure.math.combinatorics :as cmb]))
+  (:require [clojure.math.combinatorics :as cmb]
+            [clojure.set :as cs]))
 
 (def +all-perm-set+ (map #(set (cmb/permutations (range 1 (inc %))))
                          (range)))
@@ -23,7 +24,7 @@
           (map (fn [p] (set (filter #(cost? p % cost) ps)))
                ps)))
 
-(def ^:const perm-n 4)
+(def ^:const perm-n 3)
 (def pset (perm-set perm-n))
 (def edges (let [cs (range 1 perm-n)]
              (zipmap cs
@@ -35,36 +36,25 @@
             (map #(vector c %) (filter ps (es p))))
           edges))
 
-(defn waste-count [p1 p2 c ps]
-  (let [p (concat p1 (take-last c p2))]
-    (->> (map #(take-last perm-n (drop-last % p)) (range c))
-         (filter (complement ps))
-         (count))))
+(defn spm->perms [spm]
+  (->> (range)
+       (map #(take perm-n (drop % spm)))
+       (take-while #(= (count %) perm-n))
+       (set)))
 
 (defn find-superperm
-  ([spm cost waste mx-waste mx ps]
-   (cond
-     (empty? ps) {:spm spm, :cost cost, :waste waste}
-     :else (reduce (fn [m [c p]]
-                     (let [w (+ waste (waste-count (last spm) p c ps))
-                           m2 (if (< mx-waste w)
-                                {:spm spm, :cost cost, :waste waste}
-                                (find-superperm (conj spm p) (+ cost c) w mx-waste m (disj ps p)))]
-                       (if (< (:cost m) (:cost m2))
-                         m2
-                         m)))
-                   mx
-                   (next-perms (last spm) ps))))
-  ([mx-waste] (let [st (range 1 (inc perm-n))]
-        (find-superperm [st] 0 0 mx-waste {:cost 0} (disj pset st)))))
-
-(defn print-superperm [spm]
-  (apply str
-         (first
-          (reduce (fn [[r lp] p]
-                    (let [c (first (filter #(cost? lp p %) (range perm-n)))]
-                      [(concat r (take-last c p)) p]))
-                  [(first spm) (first spm)]
-                  (rest spm)))))
-
-;(find-superperm [[1 2 3]] 0 pset)
+  ([spm_ mn c p]
+   (let [spm (concat spm_ (take-last c p))
+         rest-ps (cs/difference pset (spm->perms spm))]
+     (cond
+       (< (count mn) (count spm)) mn
+       (empty? rest-ps) spm
+       :else (reduce (fn [m [c p]]
+                       (let [m2 (find-superperm spm m c p)]
+                         (if (< (count m2) (count m))
+                           m2
+                           m)))
+                     mn
+                     (next-perms (take-last perm-n spm) rest-ps)))))
+  ([] (let [st (range 1 (inc perm-n))]
+        (find-superperm [] (range 100) perm-n st))))
