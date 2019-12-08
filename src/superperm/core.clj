fcaf08b08db9d789eadbeeae88b78d4cdb6c07bd
MISUMI Masaru <deltam@users.noreply.github.com>
2019-12-09 01:40:08 +0900

    Rewrite using tree

--- src/superperm/core.clj
+++ src/superperm/core.clj
@@ -16,8 +16,8 @@
 
 
 
-(def ^:const perm-n 4)
-(def upper-limit
+(def ^:const perm-n 3)
+(def ^:const upper-limit
   "n! + (n-1)! + (n-2)! + (n-3)! + n - 3
   https://www.gregegan.net/SCIENCE/Superpermutations/Superpermutations.html#WILLIAMS"
   (let [fn-3 (apply * (range 1 (- perm-n 2)))
@@ -28,37 +28,104 @@
 
 (def pset (perm-set perm-n))
 
-(defn cost [p1 p2]
+(defn raw-cost [p1 p2]
   (first
    (filter #(= (drop % p1) (drop-last % p2))
            (range 1 (inc perm-n)))))
+(def cost (memoize raw-cost))
 
-(def edges-cost (let [es (for [x pset, y pset :when (not= x y)]
-                           [(cost x y) x y])]
-                  (zipmap (map rest es) (map first es))))
 
-(defn cost-under? [vs]
-  (every? #(< % perm-n)
-          (map cost vs (rest vs))))
 
-(defn vec->spm [vs]
-  (reduce (fn [spm p]
-            (let [c (cost (take-last perm-n spm) p)]
-              (concat spm (take-last c p))))
-          (first vs)
-          (rest vs)))
+;; functional tree
 
-(def all-spm (let [st (range 1 (inc perm-n))]
-               (->> (cmb/permutations (disj pset st))
-                    (filter cost-under?)
-                    (map #(vec->spm (concat [st] %))))))
+(defn rep-tree [f v]
+  [v (fn [] (map #(rep-tree f %)
+                 (f v)))])
 
-(defn min-spm []
-  (let [cut (filter #(<= (count %) upper-limit) all-spm)
-        sorted (sort #(< (count %1) (count %2)) cut)
-        mn (first sorted)]
-    (take-while #(= (count %) (count mn)) sorted)))
+(defn tval [t] (first t))
+(defn branch [t] ((second t)))
 
+(declare reduce-tree-bs)
+(defn reduce-tree [f g init t]
+  (f (reduce-tree-bs f g init (branch t))
+     (tval t)))
+(defn reduce-tree-bs [f g init bs]
+  (if (empty? bs)
+    init
+    (g (reduce-tree f g init (first bs))
+       (reduce-tree-bs f g init (rest bs)))))
+
+(defn take-while-tree [pred t]
+  (cond
+    (empty? t) t
+    (pred (tval t)) []
+    :else [(tval t) (fn [] (filter not-empty
+                                   (map #(take-while-tree pred %) (branch t))))]))
+
+
+
+;; superperm tree
+
+(defn gen-spm-branch [[prefix rest-ps]]
+  (map #(let [c (cost (take-last perm-n prefix) %)]
+          [(concat prefix (take-last c %)) (disj rest-ps %)])
+       rest-ps))
+
+(def spm-tree (let [st (range 1 (inc perm-n))]
+                (rep-tree gen-spm-branch [st (disj pset st)])))
+
+(defn min-count [a b]
+  (if (< (count a) (count b))
+    a
+    b))
+
+(defn find-min-spm [t]
+  (->> t
+       (take-while-tree (fn [[spm _]] (< upper-limit (count spm))))
+       (reduce-tree (fn [mn [spm ps]] (if (empty? ps)
+                                        (min-count spm mn)
+                                        mn))
+                    min-count
+                    (range upper-limit))))
+
+
+(defn filter-min-count [vs]
+  (let [mn (reduce min-count (first vs) (rest vs))]
+    (filter #(<= (count %) (count mn)) vs)))
+
+(defn find-min-spms [t]
+  (->> t
+       (take-while-tree (fn [[spm _]] (< upper-limit (count spm))))
+       (reduce-tree (fn [ms [spm ps]] (if (empty? ps)
+                                        (conj ms spm)
+                                        ms))
+                    (fn [ms spms] (filter-min-count (concat ms spms)))
+                    [])))
+
+
+
+;; utils
+
+(defn prune [n t]
+  (if (<= n 0)
+    []
+    [(tval t) (fn [] (filter not-empty
+                             (map #(prune (dec n) %) (branch t))))]))
+
+(defn get-tree [t [k & ks]]
+  (let [bs (branch t)]
+    (cond
+      (nil? k) t
+      (empty? bs) []
+      :else (get-tree (nth bs k) ks))))
+
+(defn count-leaf [t]
+  (reduce-tree (fn [cnt [spm ps]] (if (empty? ps)
+                                    (inc cnt)
+                                    cnt))
+               +
+               0
+               t))
 
 
 
